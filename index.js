5279e01bb75e8dee0ca1374696da744dd5be2ac3
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-02 11:55:26 -0800

    async loading

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 General Electric Company. All rights reserved.
+ * The copyright to the computer software herein is the property of
+ * General Electric Company. The software may be used and/or copied only
+ * with the written permission of General Electric Company or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the software has been supplied.
+ *
+ * author: apolo.yasuda@ge.com
+ */
+ 
+(src)=> {
+    return new Promise(function (resolve, reject) {
+        var s;
+        s = document.createElement('script');
+        s.src = src;
+        s.onload = resolve;
+        s.onerror = reject;
+        document.head.appendChild(s);
+    });
+}(cdnSource)
+    .catch(loadScript.bind(null, localSource))
+    .then(successCallback, failureCallback);
