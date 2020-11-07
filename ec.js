2eed585064795a7e6819a2dafff221b518c4a94e
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-07 11:25:38 -0800

    Update ec.js

--- ec.js
+++ ec.js
@@ -2,11 +2,14 @@ class EC extends Base {
   constructor(id) {
       super();
       console.log(`EC id# ${id}`);
-      this.Api();
+      this.Api().then((resp)=>{
+ console.log(`Api(): ${resp}`);
+}).catch((e)=>{
+ console.log(`Exception: ${e}`);
+});
   }
   
-  async Api(){
-      const response = await fetch('https://api.github.com/repos/ec-release/web-ui/contents/webui-assets/godoc');
-      const data = await response.json();  
+  Api(){
+    return fetch('https://api.github.com/repos/ec-release/web-ui/contents/webui-assets/godoc');
   }
 }
