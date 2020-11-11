6a30e99ab6b668713b71777741d6e20c9ba87ebe
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-11 15:27:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -34,10 +34,17 @@ class Base {
         history.pushState({}, {}, uri);
     }
     getBoolIcon(ok,uri) {
-        return `<a href="${assetPath+'/'+uri}" class="ec-godoc-rev">`
-              +`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">`
+        let html='';
+        if (ok){
+            html+=`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">`
               +`<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>`
               +`<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/></svg></a>`;
+        } else {
+            html+=`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">`
+              +`<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>`
+              +`<path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>`;
+        }
+        return `<a href="${assetPath+'/'+uri}" class="ec-godoc-rev">${html}</a>`;
     }
 }
 
@@ -88,11 +95,11 @@ class Base {
                                    + `</tr></thead><tbody>`;
                     for (let file of data) {
                         if (file.type == "dir") {
-                            htmlString += `<tr><th scope="row">${file.name}</th>`
+                            htmlString += `<tr class="text-center"><th scope="row">${file.name}</th>`
                                         + `<td>${ec.getBoolIcon(true,file.path)}</td>`
+                                        + `<td>${ec.getBoolIcon(false,file.path)}</td>`
                                         + `<td>${ec.getBoolIcon(true,file.path)}</td>`
-                                        + `<td>${ec.getBoolIcon(true,file.path)}</td>`
-                                        + `<td>${ec.getBoolIcon(true,file.path)}</td>`
+                                        + `<td>${ec.getBoolIcon(false,file.path)}</td>`
                                         + `</tr>`;
                         }
                     }
