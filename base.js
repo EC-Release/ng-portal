d235aa71358c8a611152fe2ee20242ceb18a958d
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-18 22:59:24 -0800

    Create base.js

---
+++ base.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020 General Electric Company. All rights reserved.
+ * The copyright to the computer software herein is the property of
+ * General Electric Company. The software may be used and/or copied only
+ * with the written permission of General Electric Company or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the software has been supplied.
+ *
+ * author: apolo.yasuda@ge.com
+ */
+ 
+class Base {
+    constructor() {}
+    load(src) {
+        return new Promise((resolve,reject)=>{
+            var s;
+            s = document.createElement('script');
+            s.src = src;
+            s.onload = resolve;
+            s.onerror = reject;
+            document.head.appendChild(s);
+        }
+        );
+    }
+    routing(){
+        let op=window.location.pathname;
+        switch (op) {
+          case appPath+'/godoc':
+            $('ul > li.ec-godoc').click();
+            break;
+          case appPath+'/releases':
+            $('ul > li.ec-releases').click();
+            break;
+          case appPath+'/security':
+            $('ul > li.ec-security').click();
+            break;
+          default:
+        }
+    }
+    
+    setActiveTab(elm) {
+        $("ul>li>a.active").removeClass("active");
+        $(elm).addClass('active');
+        this.setActiveState(elm,$(elm).attr('href'));
+    }
+    setActiveState(elm,uri) {
+        history.pushState({}, {}, uri);
+    }
+    getBoolIcon(ok,uri) {
+        let html='';
+        if (ok){
+            html+=`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">`
+              +`<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>`
+              +`<path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/></svg></a>`;
+        } else {
+            html+=`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">`
+              +`<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>`
+              +`<path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>`;
+        }
+        return `<a href="${assetPath+'/'+uri}" class="ec-godoc-rev">${html}</a>`;
+    }
+}
+
+export { Base as default };
