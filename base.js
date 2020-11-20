17eb20530d1a64d604328a379a222ce0660f6767
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-20 08:26:06 -0800

    Update base.js

--- base.js
+++ base.js
@@ -57,7 +57,7 @@ class Base {
               +`<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>`
               +`<path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>`;
         }
-        return `<a href="${assetPath+'/'+uri}" class="ec-godoc-rev">${html}</a>`;
+        return `<a href="${this.assetPath}/${uri}" class="ec-godoc-rev">${html}</a>`;
     }
 }
 
