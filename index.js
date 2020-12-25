74febcd4466c5b57c768a58b848592f02ea756f3
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-25 01:04:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -124,9 +124,9 @@ import define from "./analytics.js";
                       .css({position: "fixed",left: $('body')[0].getBoundingClientRect().width-100, bottom: 20, color: 'grey'}).text('[ + data ]')
                       .on( "click", ()=>{
                     $('body').append($('<div class="ec-block"></div>')
-                      .css({width:$('body')[0].getBoundingClientRect().width,height:$('body')[0].getBoundingClientRect().height,opacity:0.8,top:$('body')[0].getBoundingClientRect().top,left:$('body')[0].getBoundingClientRect().left,position:"absolute",'pointer-events': 'none',"z-index":5000,display:"block",background:"black"})
-                      .on("click", ()=>{
-                        
+                      .css({width:$('body')[0].getBoundingClientRect().width,height:$('body')[0].getBoundingClientRect().height,opacity:0.8,top:$('body')[0].getBoundingClientRect().top,left:$('body')[0].getBoundingClientRect().left,position:"absolute","z-index":5000,display:"block",background:"black"})
+                      .on("click", (e)=>{
+                      
                       //$('.ec-info').remove();
                       $('.ec-data-model').remove();
                       $('.ec-block').remove();
