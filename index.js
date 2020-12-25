0a35ab402cae3f114d7b4263603d4b7a3fd38d0e
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-24 22:01:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ import define from "./analytics.js";
                       $('.ec-data-model').remove();
                       $('.ec-block').remove();
                       
-                    });
+                    }));
                     $('body').append($('<div class="ec-data-model"></div>')
                       .css({width: 640,height: 480,position: 'fixed',top: '50%',left: '50%';transform: 'translate(-50%, -50%)';'z-index': 5001,'background-color': 'whitesmoke';'border-radius': 3}));
                 }));
