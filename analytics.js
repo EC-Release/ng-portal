0db8ffcaee9052b6765d1df256db9b5c2995f06b
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-24 16:18:16 -0800

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -116,9 +116,9 @@ This variant of a [sunburst diagram](/@d3/sunburst) shows only two layers of the
 }
 );
   main.variable(observer("data")).define("data", ["FileAttachment"], function(FileAttachment){
-   return FileAttachment("flare-2.json").json();
+   //return FileAttachment("flare-2.json").json();
    
-   //return window.ngData;
+   return window.ngData;
   });
  
   main.variable(observer("partition")).define("partition", ["d3"], function(d3){return(
