6e78d7c8048c0e65d39698e811a3c059d64fe5f8
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-05 10:30:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ import EC from './ec.js'
 
                 $("main").html(`<div class="container p-0">
 
-		<h1 class="h3 mb-3">Messages</h1>
+		<h1 class="h3 mb-3">EC Communicator (WIP)</h1>
 
 		<div class="card">
 			<div class="row g-0">
