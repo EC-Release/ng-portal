cd684f117a0317a1c6e3af22c0921a25f1ad4fa9
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-06 10:49:55 -0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,13 @@
-onmessage = (e)=>{
-  console.log(`execute ec worker with detail ${e}`);
-  location.reload();
-  postMessage({"status":"ok"});
+onconnect = (e)=>{
+    let port = e.ports[0];
+
+    /*port.onmessage = function(e) {
+      var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+      port.postMessage(workerResult);
+    }*/
+
+    port.start();
+    setTimeInterval(()=>{
+      port.postMessage({status:'ok',time: new Date().toLocaleTimeString()});
+    },3000);
 }
