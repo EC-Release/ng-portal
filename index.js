6afe0acdeb97fafd1520d094824ef4b09ee8ce1d
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-09 15:58:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -157,6 +157,14 @@ import EC from './ec.js'
                     return '&nbsp;';
                 }
 		
+		let remoteOps = (node)=>{
+                    if (node.startsWith(`https://${ec.appHost}`)){
+                        return `<a class="ec-seed-remote" href="javascript:void(0)">${feather.icons['monitor'].toSvg({'color':'blue'})}</a>`;
+                    }
+                    
+                    return '&nbsp;';
+                }
+		
 		//ec.attachWorker(`${ec.assetPath}/worker.js`);
                 
                 ec.TenguAPI('seed', '', 'GET').then(data=>{
@@ -168,6 +176,7 @@ import EC from './ec.js'
                                     `<th scope="col">Status</th>` + 
                                     `<th scope="col">Retry</th>` + 
                                     `<th scope="col">Reboot</th>` + 
+                                    `<th scope="col">Remote</th>` + 
                                     `<th scope="col" class="text-left">Updated On</th>` + 
                                     `<th scope="col" class="text-left">Joined On</th>` + 
                                     `</tr></thead><tbody>`;
@@ -185,6 +194,7 @@ import EC from './ec.js'
                             `<td>${st(seed.Status)}</td>` + 
                             `<td>${seed.Retry}</td>` + 
                             `<td>${refreshOps(seed.Node)}</td>` + 
+                            `<td>${remoteOps(seed.Node)}</td>` + 
                             `<td>${tc(seed.UpdatedOn*1000)}</td>` + 
                             `<td>${tc(seed.CreatedOn*1000)}</td></tr>`;
 		    });
