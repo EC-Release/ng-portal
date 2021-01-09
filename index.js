e3aa481a685c95d861cc840bd3dd4e7bd6ddc70a
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-09 11:55:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -16,68 +16,52 @@ import EC from './ec.js'
     let _s = window.location.pathname.split('/'),
         ec = new EC('ec1',window.location.host,_s[1],_s[2]);
     window.ec = ec;
-    ec.load("https://code.jquery.com/jquery-3.5.1.slim.min.js").catch((err)=>{}
-    ).then((s)=>{        
-        ec.windowEventBinder();
-        
-        ec.load("https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js").catch((err)=>{}
-        ).then((success)=>{
+    ec.load("https://code.jquery.com/jquery-3.5.1.slim.min.js").catch((err)=>{}).then((s)=>{ 
+	    ec.windowEventBinder();
+	    
+	    ec.load("https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js").then((success)=>{		  
 
-            showdown.extension('header-anchors', ()=>{
+		    showdown.extension('header-anchors', ()=>{	
+			var ancTpl = '$1<a id="user-content-$3" class="anchor" href="#$3" aria-hidden="true"><svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>$4';
+			return [{
+				type: 'html',
+				regex: /(<h([1-5]) id="([^"]+?)">)(.*<\/h\2>)/g,
+				replace: ancTpl
+			}];
+		    });
+          		
+		    let marked = new showdown.Converter({           	
+			    extensions: ['header-anchors'],
+			    ghCompatibleHeaderId: true
+		    });
+                                                       
+		    let ay=ec.getNgObjVal('ay'),				   
+			data1=ec.getNgObjVal('ip');                               
 
-                var ancTpl = '$1<a id="user-content-$3" class="anchor" href="#$3" aria-hidden="true"><svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>$4';
-
-                return [{
-                    type: 'html',
-                    regex: /(<h([1-5]) id="([^"]+?)">)(.*<\/h\2>)/g,
-                    replace: ancTpl
-                }];
-            });
-
-            let marked = new showdown.Converter({
-                extensions: ['header-anchors'],
-                ghCompatibleHeaderId: true
-            });
-            
-            ec.TenguAPI('', '', 'GET').then(data1=>{
-                for (const val of data1) {
-                    ec.TenguAPI(val, '', 'GET').then(data=>{
-                        if (ec.ngObjSize>=data1.length) {
-                            console.log(`all keys: ${data1} added. continue geo analysis.`);
-                            return ec.TenguAPI('ip', '', 'GET').then(data1=>{
-                                let ay=ec.getNgObjVal('ay');
-                                data1.list.split(', ').forEach((ip)=>{
-                                    if (!ip.startsWith('10.')){
-                                        ec.Api(`${ay.cred.ipdata.url}/${ip}?${ay.cred.ipdata.key}=${ay.cred.ipdata.value}`).then((data)=>{                                   
-                                            //console.log(`geo svc: ${data} browsHistory: ${ec.getNgObjVal('browseHistory')}`);
-                                            let bh = ec.getNgObjVal('browseHistory'),
-                                                ts = (new Date()).getTime();
-                                            bh.list[`${ts}`]={
-                                                ip:ip,
+		    data1.list.split(', ').forEach((ip)=>{                                    
+			 if (!ip.startsWith('10.')){
+				 ec.Api(`${ay.cred.ipdata.url}/${ip}?${ay.cred.ipdata.key}=${ay.cred.ipdata.value}`).then((data)=>{                                                                    
+					 //console.log(`geo svc: ${data} browsHistory: ${ec.getNgObjVal('browseHistory')}`);                                         
+					 let bh = ec.getNgObjVal('browseHistory'),
+                                                ts = (new Date()).getTime();                                         
+					 bh.list[`${ts}`]={                                       
+						ip:ip,
                                                 lat:data.latitude,
                                                 lng:data.longitude,
                                                 city:data.city,
                                                 country:data.country_name,
                                                 zip:data.postal,
                                                 state:data.region_code
-                                            };
-                                            return ec.TenguAPI('browseHistory',bh,'POST').then((data)=>{
-                                                //console.log(`geolocation updated. ${JSON.stringify(data)}`);
-                                                ec.routing();
-                                            });
-                                        }).catch(e=>{
-                                            console.log(`Exception: ${e}`);
-                                        });
-                                    }
-                                });
-                               
-                            }).catch((e)=>{
-                                throw e;
-                            });                
-                        }
-                    }).catch(e=>{
-                        console.log(`Exception: ${e}`);
-                    });
+                                            };                                            
+					 return ec.TenguAPI('browseHistory',bh,'POST').then((data)=>{                                         
+						 //console.log(`geolocation updated. ${JSON.stringify(data)}`);                                                
+						 ec.routing();
+					 });                                        
+				 }).catch(e=>{                                 
+					 console.log(`Exception: ${e}`);                                        
+				 });                                 
+			 }                         
+		    });                               
                 }
             }).catch((e)=>{
                 console.log(`Exception: ${e}`);
@@ -86,7 +70,6 @@ import EC from './ec.js'
             $('ul').on('click', 'li.ec-godoc', (event)=>{
 
                 ec.setActiveTab(event.target,`${ec.appPath}/godoc`);
-
                 event.preventDefault();
                 if (ec.sdkInnerHTML != "") {
                     $("main").html(ec.sdkInnerHTML);
