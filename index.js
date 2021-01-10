d96d7d0cbeaa8823b36ac7efb99a2746a8e7346e
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-09 16:18:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ import EC from './ec.js'
 			
 		    $('.ec-seed-remote > svg').on('click',(e)=>{
                         e.preventDefault();
-			ec. showTerminal(`/${ec.appPath}/log`);
+			ec. showTerminal(`${ec.appPath}/log`);
 		    });
                     
                     $(event.target).addClass('active');
