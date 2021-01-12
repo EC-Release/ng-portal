7300f12922b749b05188b3a7c68e434ef6baf168
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-12 15:18:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -624,7 +624,7 @@ import EC from './ec.js'
 		}).then(rid=>{
 			return ec.Api(`https://ge-dw.aha.io/api/v1/releases/${rid}/features`,h);
 		}).then(fs=>{
-			let htmlString = "";
+			let htmlString = `<div class="row justify-content-around"><div class="col-4">`;
 			fs.features.forEach((ft,idx)=>{
 				ec.Api(`https://ge-dw.aha.io/api/v1/features/${ft.id}`,h).then(f=>{
 					htmlString += `<div class="card bg-light mb-3" style="max-width: 18rem;">` +
@@ -635,6 +635,7 @@ import EC from './ec.js'
 							 `</div>` +
 							 `</div>`;
 					if (idx==fs.features.length-1){
+						htmlString += `</div><div class="col-4">content</div></div>`;
 						ec.setActiveTab(event.target,`${ec.appPath}/features`);
 						ec.featureHTML = htmlString;
 						$("main").html(ec.featureHTML);
@@ -642,6 +643,8 @@ import EC from './ec.js'
 				}).catch(e=>{console.log(e)});
 				
 			});
+			   
+			
 		}).catch(e=>{console.log(e)});
 		    
                 
