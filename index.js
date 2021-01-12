f9c2be94f74399eed90dbc960df826c0f8aae503
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-12 15:31:33 -0800

    Update index.js

--- index.js
+++ index.js
@@ -635,7 +635,19 @@ import EC from './ec.js'
 							 `</div>` +
 							 `</div>`;
 					if (idx==fs.features.length-1){
-						htmlString += `</div><div class="col-4">content</div></div>`;
+						htmlString += `</div><div class="col-4"><div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+								  <div class="card-header">Header</div>
+								  <div class="card-body">
+								    <h5 class="card-title">Dark card title</h5>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								  </div>
+								</div></div><div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+								  <div class="card-header">Header</div>
+								  <div class="card-body">
+								    <h5 class="card-title">Secondary card title</h5>
+								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								  </div>
+								</div></div>`;
 						ec.setActiveTab(event.target,`${ec.appPath}/features`);
 						ec.featureHTML = htmlString;
 						$("main").html(ec.featureHTML);
