abd9526b5c05f4bbc10ac3778a94742b97c558cc
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-20 18:59:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ import EC from './ec.js'
 		let htmlString = "";
 		htmlString += `<table class="table text-center table-striped"><caption>Script Scheduler</caption><thead><tr>` +                 
                                         `<th scope="col" class="text-left">git commit</th>` + 
-                                        `<th scope="col">source vendor</th>` +
+                                        `<th scope="col">repo vendor</th>` +
                                         `<th scope="col">time</th>` + 
                                         `<th scope="col">freq</th>` + 
                                         `<th scope="col">owner</th>` + 
@@ -317,6 +317,36 @@ import EC from './ec.js'
                         htmlString += '</table>';
 
                 $("main").html(htmlString);
+		let frm=`<form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>`;
                 $(event.target).addClass('active');
             });
 		    
