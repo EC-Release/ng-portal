04763518254adc390d86befcadba11954c271963
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-20 20:15:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -275,12 +275,12 @@ import EC from './ec.js'
 			
 		    $('.ec-seed-debug > svg').on('click',(e)=>{
                         e.preventDefault();
-			ec. showRemoteDebug(`wss://${ec.appHost+ec.appPath}/log`);
+			ec.showRemoteDebug(`wss://${ec.appHost+ec.appPath}/log`);
 		    });
 			
 	            $('.ec-seed-term > svg').on('click',(e)=>{
                         e.preventDefault();
-			ec. showTerminal(`wss://${ec.appHost+ec.appPath}/term`);
+			ec.showTerminal(`wss://${ec.appHost+ec.appPath}/term`);
 		    });
                     
                     $(event.target).addClass('active');
@@ -312,41 +312,16 @@ import EC from './ec.js'
                                     `<td>2021-FEB-2</td>` + 
                                     `<td>Hourly</td>` + 
                                     `<td>apolo.yasuda</td>` + 
-                                    `<td><a href="javascript:void(0);">${feather.icons['edit'].toSvg({'color':'black'})}</a></td></tr>`;
+                                    `<td><a class="ec-show-scheduler-form"" href="javascript:void(0);">${feather.icons['edit'].toSvg({'color':'black'})}</a></td></tr>`;
                         //}
                         htmlString += '</table>';
 
                 $("main").html(htmlString);
-		let frm=`<form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>`;
+		$('.ec-show-scheduler-form > svg').on('click',(e)=>{
+			e.preventDefault();
+			ec.showSchedulerForm();
+		});
+
                 $(event.target).addClass('active');
             });
 		    
