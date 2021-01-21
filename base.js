9f961233a3cf0137049bd11dd66edb6d2daa94f5
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-21 15:41:20 -0800

    Update base.js

--- base.js
+++ base.js
@@ -346,22 +346,25 @@ class Base {
             transform: 'translate(-50%, -50%)',
             'z-index': 5001,
             'background-color': 'whitesmoke',
-            'border-radius': 3,
+            'border-radius': 5,
             'padding': 20
         }).html(`<form>
   <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <label for="exampleFormControlInput1">Git Commit</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
+    <label for="exampleFormControlSelect1">Repo Vendor</label>
     <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+      <option>github</option>
+      <option>gitlab</option>
+      <option>build.ge</option>
+      <option>bitbucket</option>
     </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Git Commit</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
   </div>
   <div class="form-group">
     <label for="exampleFormControlSelect2">Example multiple select</label>
