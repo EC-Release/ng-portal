4eea3b112e0e6626e7bd127503c0283191131080
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-23 10:37:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -644,7 +644,7 @@ import EC from './ec.js'
                         color: 'grey'
                     }).text('[ + data@EC ]').on("click", (e)=>{
                         e.preventDefault();
-                        ec.TenguDataInit('qa');
+                        ec.TenguDataInit('');
                         ec.showDataModel();
                     }));
                 }
