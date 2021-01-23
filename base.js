9995256c620975da734931abc3ce2b8f656e841c
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-23 12:47:45 -0800

    Update base.js

--- base.js
+++ base.js
@@ -481,7 +481,7 @@ class Base {
             for (const _k in aq) {
                 let _v = aq[_k];
                 if (!_v.value.hasOwnProperty('parent'))
-                    console.error(`invalid keyvalue pair ${_v.value}`);
+                    console.debug(`invalid keyvalue pair ${_v.value}: no parent defined.`);
 
                 _this.TenguAPI(_v.key, _v.value, _v.method).then(data=>{
 
