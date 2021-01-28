4160356cf0fc514329e1dabaf547c1fa9e0a0071
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-27 20:46:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ import EC from './ec.js'
                     ghCompatibleHeaderId: true
                 });
 
-                let ay = ec.getNgObjVal('ay');
+                /*let ay = ec.getNgObjVal('ay');
 
                 ec.TenguAPI('ip').then(data1=>{
                     data1.list.split(', ').forEach((ip)=>{
@@ -83,7 +83,7 @@ import EC from './ec.js'
                 ).catch(e=>{
                     console.log(`get up err: ${e}`)
                 }
-                );
+                );*/
 
                 $('ul').on('click', 'li.ec-godoc', (event)=>{
 
