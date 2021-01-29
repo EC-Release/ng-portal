9d9ea8028f3b3d605947b8338a9f3fab2705722c
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-29 14:12:31 -0800

    Update ec.js

--- ec.js
+++ ec.js
@@ -252,7 +252,7 @@ class EC extends Base {
   setNgObj(key,val) {
     _val=this.getNgObjVal(key);
     if (_val.name&&_val.name=='browseHistory'){
-      _val['list']&&_val['list'][`${Date.now()}`]=val;
+      _val['list']&&(_val['list'][`${Date.now()}`]=val);
     }
           
     this.#ngObj.set(key,_val);
