955c911bcf2106492dec95b13ed628104c747fd8
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-30 09:27:53 -0800

    Update ec.js

--- ec.js
+++ ec.js
@@ -25,6 +25,7 @@ class EC extends Base {
       this.appRev = rev;
       this.appPath = `/${this.appRev}/${path}`;
       this.apiPath = `/${this.appRev}/${path}/${api}`;
+      this.appExit = `/${this.appRev}/${path}/exit`;
       this.appExec = `/${this.appRev}/${path}/exec`;
       this.appTerm = `/${this.appRev}/${path}/term`;
       this.appLog = `/${this.appRev}/${path}/log`;
